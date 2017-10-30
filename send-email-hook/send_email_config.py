27e3000a8f25ea8a32e94aafdf0800543a0176f0
cqupt-gsy <gaosy0106@gmail.com>
2017-10-30 17:46:10 +0800

    add send email code for python

---
+++ send-email-hook/send_email_config.py
@@ -0,0 +1,16 @@
+DEFAULT_COMMIT_MSG_NUMBER = '1'
+
+USER_NAME = 'Shiyi'
+EMAIL_ACCOUNT = '{}@server.com'.format(USER_NAME)
+EMAIL_SERVER_HOST = 'server.smtp.com'
+EMAIL_SERVER_HOST = 25
+
+EMAIL_NOTIFY_LIST_SYS = ['email1@mail.com', 'email2@mail.com']
+EMAIL_NOTIFY_LIST_UAT = ['email1@mail.com', 'email2@mail.com', 'email3@mail.com']
+
+ENVIRONMENT_LIST = ['SYS', 'UAT']
+WAITING_TIME_LIST = ['5', '10']
+
+SUBJECT = 'Email subject'
+
+EMAIL_MESSAGE_TEMPLATE = 'From: {}\nTo: {}\nSubject: {}\n\n{}\n\nThanks\n{}'

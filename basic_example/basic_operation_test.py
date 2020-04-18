60edf71e5d4dfe8152a11094518a9f570a2ff70b
Shiyi.GAO <gaosy.work@gmail.com>
2020-04-18 17:09:08 +0800

    [Shiyi] restructure projects

--- basic_example/basic_operation_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from basic_operation import BasicOperation
-
-
-class BasicOperationTest(unittest.TestCase):
-    def __init__(self):
-        self.basicOperation = BasicOperation
-        pass
